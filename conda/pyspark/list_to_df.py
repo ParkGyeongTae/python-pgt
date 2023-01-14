72de847720d2cb863069d8f802d3285016f30d02
ParkGyeongTae <pgt0409@gmail.com>
2023-01-14 19:26:57 +0900

    add list_to_df

---
+++ conda/pyspark/list_to_df.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+spark = SparkSession \
+        .builder \
+        .master('local') \
+        .appName('my_pyspark_app') \
+        .getOrCreate()
+
+data = [
+    ("kim", 100),
+    ("kim", 90),
+    ("lee", 80),
+    ("lee", 70),
+    ('park', 60)
+    ]
+
+schema = StructType([ \
+    StructField('name', StringType(),True), \
+    StructField('score', IntegerType(),True)
+    ])
+ 
+df = spark.createDataFrame(data = data, schema = schema)
+
+df.printSchema()
+df.show()
