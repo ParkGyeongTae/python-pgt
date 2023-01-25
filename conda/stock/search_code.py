1779ee430e2327a03cf2f3f8f1044a5066212368
ParkGyeongTae <pgt0409@gmail.com>
2023-01-25 20:02:36 +0900

    modify get kosdaq

--- conda/stock/search_code.py
+++ conda/stock/search_code.py
@@ -4,17 +4,29 @@ pip install pandas,beautifulsoup4,finance-datareader -y
 
 import FinanceDataReader as fdr
 
-def search_code(name):
+def get_stock_code(name):
     df = fdr.StockListing('KRX')
-    code = df[df['Name'] == name]['Code'][0]
-    return code
+    stock_code = df[df['Name'] == name]['Code'].to_string(index = False)
+    return stock_code
+
+def get_kospi_dataframe():
+    df = fdr.StockListing('KRX')
+    df = df[df['Market'] == 'KOSPI']
+    return df
+
+def get_kosdaq_dataframe():
+    df = fdr.StockListing('KRX')
+    df = df[df['Market'] == 'KOSDAQ']
+    return df
 
 if __name__ == '__main__':
-    samsung_electronics_code = search_code('삼성전자')
-    print(samsung_electronics_code)
+    df_kosdaq = get_kosdaq_dataframe()
+    print(df_kosdaq.head(20))
 
-
-
+    # code = get_stock_code('LG에너지솔루션')
+    # df = fdr.DataReader(symbol = code, start = '2023-01-10', end = '2023-01-20')
+    # print(df)
+    # print(fdr.StockListing('KRX'))
 
 # df_krx = fdr.StockListing('KRX')
 # print(df_krx.head())
