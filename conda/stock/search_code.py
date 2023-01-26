91866ebc4c8eab8b0e7f9fb6edc4be8f5461699f
ParkGyeongTae <pgt0409@gmail.com>
2023-01-26 19:54:24 +0900

    파일 분할

--- conda/stock/search_code.py
+++ conda/stock/search_code.py
@@ -2,6 +2,9 @@
 pip install pandas,beautifulsoup4,finance-datareader -y
 '''
 
+# - 각종 지수 : KS11(코스피 지수), KQ11(코스닥 지수), DJI(다우 지수), IXIC(나스닥 지수), US500(S&P5000)
+# - 환율 데이터 : USD/KRX(원달러 환율), USD/EUR(달러당 유로화 환율), CNY/KRX(위완화 원화 환율)
+
 import FinanceDataReader as fdr
 
 def get_stock_code(name):
@@ -20,9 +23,10 @@ def get_kosdaq_dataframe():
     return df
 
 if __name__ == '__main__':
-    df_kosdaq = get_kosdaq_dataframe()
-    print(df_kosdaq.head(20))
-
+    # df_kosdaq = get_kosdaq_dataframe()
+    # print(df_kosdaq.head(20))
+    df = fdr.DataReader('KS11')
+    print(df)
     # code = get_stock_code('LG에너지솔루션')
     # df = fdr.DataReader(symbol = code, start = '2023-01-10', end = '2023-01-20')
     # print(df)
