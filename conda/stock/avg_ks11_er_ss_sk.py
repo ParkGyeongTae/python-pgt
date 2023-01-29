61d67b9e358ab5956885ec88728931f7237e247f
ParkGyeongTae <pgt0409@gmail.com>
2023-01-29 16:34:34 +0900

    주식과 환율 비교 추가

---
+++ conda/stock/avg_ks11_er_ss_sk.py
@@ -0,0 +1,49 @@
+'''
+pip install pandas,beautifulsoup4,finance-datareader -y
+'''
+
+import pandas as pd
+import FinanceDataReader as fdr
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+def get_stock_code(name):
+    '''이름을 입력하면 코드를 리턴'''
+
+    df = fdr.StockListing('KRX')
+    stock_code = df[df['Name'] == name]['Code'].to_string(index = False)
+    return stock_code
+
+def get_kospi_dataframe():
+    '''코스피를 리턴'''
+
+    df = fdr.StockListing('KRX')
+    df = df[df['Market'] == 'KOSPI']
+    return df
+
+if __name__ == '__main__':
+
+    before_one_week = (datetime.now() - relativedelta(years = 3)).strftime('%Y-%m-%d')
+
+    df_er = fdr.DataReader(symbol = 'USD/KRW', start = before_one_week)[['Close']]
+    df_ks11 = fdr.DataReader(symbol = 'KS11', start = before_one_week)[['Close']]
+    df_samsung = fdr.DataReader(symbol = get_stock_code('삼성전자'), start = before_one_week)[['Close']]
+    df_sk = fdr.DataReader(symbol = get_stock_code('SK하이닉스'), start = before_one_week)[['Close']]
+
+    moving_average_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 30, 40, 50, 60, 120, 200, 240, 360, 400, 480, 600, 720]
+
+    result_dict = {}
+
+    for day in moving_average_list:
+        result_dict[f'avg_{day}'] = [int(df_er.tail(day).sum() / day), int(df_ks11.tail(day).sum() / day), int(df_samsung.tail(day).sum() / day), int(df_sk.tail(day).sum() / day)]
+
+    df_result = pd.DataFrame(result_dict).T
+    df_result.columns = ['usd/krw', 'kospi', 'samsung', 'sk']
+
+    df_result['usd/krw_per'] = round(df_result['usd/krw'] / df_result['usd/krw'].iloc[0], 2)
+    df_result['kospi_per'] = round(df_result['kospi'] / df_result['kospi'].iloc[0], 2)
+    df_result['samsung_per'] = round(df_result['samsung'] / df_result['samsung'].iloc[0], 2)
+    df_result['sk_per'] = round(df_result['sk'] / df_result['sk'].iloc[0], 2)
+
+    print(df_result)
