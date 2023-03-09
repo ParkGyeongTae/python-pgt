9d9e10670587ea04da7af2e23e50603d66af91ff
ParkGyeongTae <pgt0409@gmail.com>
2023-03-09 20:03:32 +0900

    코드를 실행가능하도록 절대경로 추가

--- conda/stock/ohlc/sectors/electrical_appliances/0_all_stocks.py
+++ conda/stock/ohlc/sectors/electrical_appliances/0_all_stocks.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))))
 
 from main.ohlc_chart import OhlcChart
 
