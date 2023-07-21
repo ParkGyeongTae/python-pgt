3f38bc568c3b46fb5e746d686ecfa355b3d2e142
ParkGyeongTae <pgt0409@gmail.com>
2023-07-21 23:19:01 +0900

    2_isinstance 저장

---
+++ conda/practice/2_isinstance.py
@@ -0,0 +1,17 @@
+temp_str = "park"
+temp_int = 15
+temp_float = 1.53
+
+temp_list = [1, 2]
+temp_dict = {"a": 1, "b": 2}
+temp_set = {1,2,3}
+temp_tuple = (1,2,3)
+
+print(isinstance(temp_str, tuple))
+print(isinstance(temp_int, tuple))
+print(isinstance(temp_float, tuple))
+
+print(isinstance(temp_list, tuple))
+print(isinstance(temp_dict, tuple))
+print(isinstance(temp_set, tuple))
+print(isinstance(temp_tuple, tuple))
